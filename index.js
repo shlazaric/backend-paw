c4eded9c6adadadbbcf08bfb0a2b5264205a006b
Shana Antonia <95529289+shlazaric@users.noreply.github.com>
2025-11-14 15:14:08 +0100

    inicijalizacija projekta

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.listen(PORT, error => {
+    if (error) {
+        console.error(`Greška prilikom pokretanja poslužitelja: ${error.message}`);
+    } else {
+        console.log(`Server je pokrenut na http://localhost:${PORT}`);
+    }
+});
