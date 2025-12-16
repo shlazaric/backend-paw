e2e2c7254a40fba28d88e0d0a246d4719e5feff9
Shana Antonia <95529289+shlazaric@users.noreply.github.com>
2025-12-17 00:14:47 +0100

    index.js

--- index.js
+++ index.js
@@ -8,15 +8,48 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+
 async function startServer() {
     await connectToDatabase();
 
+
     app.get("/", (req, res) => {
-        res.send("PawfectStay backend radi s bazom ✅");
+        res.send("PawfectStay backend radi s MongoDB ✅");
     });
 
+    app.post("/dogs", async (req, res) => {
+        try {
+            const db = getDb();
+            const dog = req.body;
+
+            if (!dog.name || !dog.breed || !dog.age) {
+                return res.status(400).json({ message: "Nedostaju podaci o psu" });
+            }
+
+            const result = await db.collection("dogs").insertOne(dog);
+            res.status(201).json({
+                message: "Pas uspješno dodan",
+                id: result.insertedId,
+            });
+        } catch (error) {
+            res.status(500).json({ message: "Greška na serveru" });
+        }
+    });
+
+    // Dohvat svih pasa (ADMIN)
+    app.get("/dogs", async (req, res) => {
+        try {
+            const db = getDb();
+            const dogs = await db.collection("dogs").find().toArray();
+            res.json(dogs);
+        } catch (error) {
+            res.status(500).json({ message: "Greška pri dohvaćanju pasa" });
+        }
+    });
+
+
     app.listen(PORT, () => {
-        console.log(`Server pokrenut na http://localhost:${PORT}`);
+        console.log(`🚀 Server radi na http://localhost:${PORT}`);
     });
 }
 
